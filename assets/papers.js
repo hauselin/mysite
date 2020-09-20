eb144f380a928daccb61c0c0313a089c56e3abb2
hauselin <hauselin@gmail.com>
2020-09-20 19:18:01 -0400

    move papers to papers.js

---
+++ assets/papers.js
@@ -0,0 +1,50 @@
+// https://stackoverflow.com/questions/17929356/html-datalist-values-from-array-in-javascript
+
+// list of papers
+var paper_datalist_options = [
+    {
+        "option": "Lin et al 2020 Promise and perils of experimentation: The mutual internal validity problem. Perspectives on psychological science. #philosophy",
+        "title": "Lin2020experimentation.pdf"
+    },
+    {
+        "option": "Lin et al 2020 Strong effort manipulations reduce response caution: A preregistered reinvention of the ego-depletion paradigm. Psychological science. #depletion #effort #ddm #diffusion #brms #bayes",
+        "title": "https://journals.sagepub.com/doi/pdf/10.1177/0956797620904990"
+    },
+    {
+        "option": "Lin et al 2020 (code and supplement) Strong effort manipulations reduce response caution: A preregistered reinvention of the ego-depletion paradigm. Psychological science. #depletion #effort #ddm #diffusion #brms #bayes #supplement",
+        "title": "https://github.com/hauselin/depletion_bayes"
+    },
+    {
+        "option": "Frömer et al 2020 When effort matters: Expectations of reward and efficacy guide cognitive control allocation #effort #eeg #pupil",
+        "title": "Fromer2020cognitivecontrol.pdf"
+    },
+    {
+        "option": "Landy et al 2020 Crowdsourcing hypothesis tests: Making transparent how design choices shape research results. Psychological Bulletin. #open-science #philosophy",
+        "title": "Landy2020crowdsourcing.pdf"
+    },
+    {
+        "option": "Lin & Vartanian 2018 Neuroeconomic framework for creative cognition. Perspectives on psychological science.",
+        "title": "Lin2018creativity.pdf"
+    },
+    {
+        "option": "Lin et al 2018 Midfrontal theta and pupil dilation parametrically track subjective conflict (but also surprise) during intertemporal choice. NeuroImage. #eeg #pupil",
+        "title": "Lin2018theta.pdf"
+    },
+    {
+        "option": "Lin et al 2018 (supplement) Midfrontal theta and pupil dilation parametrically track subjective conflict (but also surprise) during intertemporal choice. NeuroImage. #eeg #pupil #supplement",
+        "title": "Lin2018thetaSupp.pdf"
+    },
+    {
+        "option": "Moshontz et at 2018. The Psychological Science Accelerator: Advancing psychology through a distributed collaborative network. Advances in Methods and Practices in Psychological Science. #open-science",
+        "title": "Moshontz2018psa.pdf"
+    },
+    {
+        "option": "Francis et al 2018. Development of a within-subjectrepeated-measures ego-depletion paradigm. Social psychology. #depletion #effort",
+        "title": "Francis2018depletion.pdf"
+    },
+    {
+        "option": "Saunders et al 2017. The emotive nature of conflict monitoring in the medial prefrontal cortex. International Journal of Psychophysiology. #eeg",
+        "title": "Saunders2017emotionsMPFC.pdf"
+    }
+];
+
