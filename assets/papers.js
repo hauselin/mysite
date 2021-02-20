6a7650384b2d559c191d4a5e9bbbd3d3bed5e00c
hauselin <hauselin@gmail.com>
2021-02-20 14:26:47 -0600

    add papers

--- assets/papers.js
+++ assets/papers.js
@@ -5,43 +5,59 @@
 // title: file name in public/papers
 var paper_datalist_options = [
     {
-        "option": "Lin et al 2020 Promise and perils of experimentation: The mutual internal validity problem. Perspectives on psychological science. #philosophy",
-        "title": "Lin2020experimentation.pdf"
+        "option": "Lin et al 2021. Instilling the value of effort (Stage 1 acceptance). Nature Human Behaviour. #effort",
+        "title": "Lin2021instilling_value_effort_stage1.pdf"
     },
     {
-        "option": "Lin et al 2020 Strong effort manipulations reduce response caution: A preregistered reinvention of the ego-depletion paradigm. Psychological science. #depletion #effort #ddm #diffusion #brms #bayes",
+        "option": "Lin et al 2021. Promise and perils of experimentation: The mutual-internal-validity-problem. Perspectives on psychological science. #philosophy",
+        "title": "Lin2021experimentation.pdf"
+    },
+    {
+        "option": "Lin et al 2020. Strong effort manipulations reduce response caution: A preregistered reinvention of the ego-depletion paradigm. Psychological science. #depletion #effort #ddm #diffusion #brms #bayes",
         "title": "https://journals.sagepub.com/doi/pdf/10.1177/0956797620904990"
     },
     {
-        "option": "Lin et al 2020 (supplement & code) Strong effort manipulations reduce response caution: A preregistered reinvention of the ego-depletion paradigm. Psychological science. #depletion #effort #ddm #diffusion #brms #bayes #supplement",
+        "option": "Lin et al 2020. (supplement & code) Strong effort manipulations reduce response caution: A preregistered reinvention of the ego-depletion paradigm. Psychological Science. #depletion #effort #ddm #diffusion #brms #bayes #supplement",
         "title": "https://github.com/hauselin/depletion_bayes"
     },
     {
-        "option": "Frömer, Lin et al. 2021 Expectations of reward and efficacy guide cognitive control allocation #effort #eeg #pupil",
+        "option": "Jones et al 2021. To which world regions does that valence-dominance model of social perception apply. Nature Human Behaviour #open-science #replication",
+        "title": "Jones2021valence-dominance-social-perception.pdf"
+    },
+    {
+        "option": "Frömer, Lin et al 2021. Expectations of reward and efficacy guide cognitive control allocation. Nature Communications. #effort #eeg #pupil",
         "title": "https://rdcu.be/cfemg"
     },
     {
-        "option": "Frömer, Lin et al. 2021 (code) Expectations of reward and efficacy guide cognitive control allocation #effort #eeg #pupil",
+        "option": "Frömer, Lin et al 2021 (code). Expectations of reward and efficacy guide cognitive control allocation. Nature Communications. #effort #eeg #pupil",
         "title": "https://github.com/froemero/reward-efficacy-evc-eeg"
     },
     {
-        "option": "Landy et al 2020 Crowdsourcing hypothesis tests: Making transparent how design choices shape research results. Psychological Bulletin. #open-science #philosophy",
+        "option": "Ebersole et al 2020. Many Labs 5: Testing pre-data-collection peer review as an intervention to increase replicability #open-science #replication",
+        "title": "Ebersole2020manylabs5.pdf"
+    },
+    {
+        "option": "Chartier et al 2020. Many Labs 5: Registered replication of Albarracín et al 2008. #open-science #replication",
+        "title": "Chartier2020manylabs5.pdf"
+    },
+    {
+        "option": "Landy et al 2020. Crowdsourcing hypothesis tests: Making transparent how design choices shape research results. Psychological Bulletin. #open-science #philosophy",
         "title": "Landy2020crowdsourcing.pdf"
     },
     {
-        "option": "Anderson et al 2020 The metronome response task for measuring mind wandering: Replication attempt and extension of three studies by Seli et al. #open-science #replication",
+        "option": "Anderson et al 2020. The metronome response task for measuring mind wandering: Replication attempt and extension of three studies by Seli et al. Attention Perception & Psychophysics #open-science #replication",
         "title": "Anderson2020MRT.pdf"
     },
     {
-        "option": "Lin & Vartanian 2018 Neuroeconomic framework for creative cognition. Perspectives on psychological science.",
+        "option": "Lin & Vartanian 2018. Neuroeconomic framework for creative cognition. Perspectives on Psychological Science.",
         "title": "Lin2018creativity.pdf"
     },
     {
-        "option": "Lin et al 2018 Midfrontal theta and pupil dilation parametrically track subjective conflict (but also surprise) during intertemporal choice. NeuroImage. #eeg #pupil",
+        "option": "Lin et al 2018. Midfrontal theta and pupil dilation parametrically track subjective conflict (but also surprise) during intertemporal choice. NeuroImage. #eeg #pupil",
         "title": "Lin2018theta.pdf"
     },
     {
-        "option": "Lin et al 2018 (supplement) Midfrontal theta and pupil dilation parametrically track subjective conflict (but also surprise) during intertemporal choice. NeuroImage. #eeg #pupil #supplement",
+        "option": "Lin et al 2018 (supplement). Midfrontal theta and pupil dilation parametrically track subjective conflict (but also surprise) during intertemporal choice. NeuroImage. #eeg #pupil #supplement",
         "title": "Lin2018thetaSupp.pdf"
     },
     {
@@ -49,7 +65,7 @@ var paper_datalist_options = [
         "title": "Moshontz2018psa.pdf"
     },
     {
-        "option": "Francis et al 2018. Development of a within-subjectrepeated-measures ego-depletion paradigm. Social psychology. #depletion #effort",
+        "option": "Francis et al 2018. Development of a within-subjectrepeated-measures ego-depletion paradigm. Social Psychology. #depletion #effort",
         "title": "Francis2018depletion.pdf"
     },
     {
